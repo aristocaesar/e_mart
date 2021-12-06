d77ac9a0ccffa99effb0cec8db1d7a024dec2952
Aristo Caesar Pratama <45166689+aristocaesar@users.noreply.github.com>
2021-12-06 08:37:43 +0700

    Menyelesaikan Fitur Kategori Supplier (SELESAI) Dan Bux Fixes

--- database/e_mart_database.sql
+++ database/e_mart_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2021 at 09:32 AM
+-- Generation Time: Dec 06, 2021 at 02:35 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.25
 
@@ -43,8 +43,8 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`kode_barang`, `nama_barang`, `kategori`, `harga`, `stok`, `supplier`, `created_at`, `updated_at`) VALUES
-('BRG0001', 'Epson Printer L121 Print Only - Garansi Resmi Epson', 'K001', 1735000, 20, 'S001', '2021-11-26 00:40:08', '2021-11-25 18:37:50'),
-('BRG0002', 'Tinta Canon BLUEPRINT Refill For Printer Canon 100ml - Varian Warna', 'K002', 43000, 10, 'S002', '2021-11-26 00:40:59', '2021-11-25 18:40:11');
+('BRG0001', 'Bonkyo Wireless Optical Mouse Dan Minimalism - MSE6', 'K010', 59900, 14, 'S006', '2021-12-06 07:50:03', '2021-12-06 01:48:28'),
+('BRG0002', 'Logitech B175 Mouse Wireless untuk Windows, Mac, Linux dan ChromeOS', 'K010', 96000, 12, 'S004', '2021-12-06 07:50:03', '2021-12-06 01:48:28');
 
 -- --------------------------------------------------------
 
@@ -53,7 +53,7 @@ INSERT INTO `barang` (`kode_barang`, `nama_barang`, `kategori`, `harga`, `stok`,
 --
 
 CREATE TABLE `kategori` (
-  `kode_kategori` varchar(8) NOT NULL,
+  `id_kategori` varchar(8) NOT NULL,
   `nama_kategori` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -61,10 +61,15 @@ CREATE TABLE `kategori` (
 -- Dumping data for table `kategori`
 --
 
-INSERT INTO `kategori` (`kode_kategori`, `nama_kategori`) VALUES
-('K003', 'dsadasd'),
-('K001', 'Kamera'),
-('K002', 'Tinta');
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+('K009', 'Air Coller'),
+('K006', 'Kabel Sata'),
+('K008', 'Modem'),
+('K001', 'Monitor'),
+('K010', 'Mouse'),
+('K007', 'Printer'),
+('K005', 'Sound Speaker'),
+('K003', 'Tinta');
 
 -- --------------------------------------------------------
 
@@ -82,8 +87,14 @@ CREATE TABLE `supplier` (
 --
 
 INSERT INTO `supplier` (`id_supplier`, `nama_supplier`) VALUES
-('S001', 'PT.Centrakom'),
-('S002', 'PT.Multikom');
+('S008', 'Cv.Caesar Cell'),
+('S004', 'Cv.Utama Joyo Kencono Mandraguno'),
+('S005', 'Pt.Djarum Super Indonesia'),
+('S002', 'Pt.Gudang Ganam'),
+('S001', 'Pt.Mabuk Cinta Abadi Sentosa'),
+('S006', 'Pt.Mencari Cinta Sejati'),
+('S007', 'Pt.Player Cell'),
+('S003', 'Pt.Sesuk Sukses Sampek Tipes');
 
 -- --------------------------------------------------------
 
@@ -104,7 +115,7 @@ CREATE TABLE `toko` (
 --
 
 INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat`, `no_telp`, `updated_at`) VALUES
-('T001', 'Player Cell OKe', 'Jl. Mastrip, Krajan Timur, Sumbersari, Kec. Sumbersari, Kabupaten Jember, Jawa Timur 68121', '0', '2021-12-05 15:06:54');
+('T001', 'Player Cell', 'Jl. Mastrip, Krajan Timur, Sumbersari, Kec. Sumbersari, Kabupaten Jember, Jawa Timur 68121', '0', '2021-12-06 01:43:21');
 
 -- --------------------------------------------------------
 
@@ -142,13 +153,6 @@ CREATE TABLE `transaksi_detail` (
   `kode_barang` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `transaksi_detail`
---
-
-INSERT INTO `transaksi_detail` (`kode_transaksi`, `qyt`, `sub_total`, `kode_barang`) VALUES
-('TR756454321', 1, 43000, 'BRG0001');
-
 -- --------------------------------------------------------
 
 --
@@ -173,8 +177,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `username`, `nama_lengkap`, `password`, `role`, `no_hp`, `isAktif`, `login_terbaru`, `created_at`, `updated_at`) VALUES
-('U001', 'aristo', 'ARISTO CAESAR PRATAMA', '1', 1, '0852351191010', 'Aktif', '2021-12-05 15:29:32', '2021-12-03 01:50:51', '2021-12-05 14:19:21'),
-('U002', 'person', 'ULUL GAMING', 'person', 2, '543534534', 'Aktif', '2021-12-03 01:54:41', '2021-12-03 01:54:41', '2021-12-05 12:34:19'),
+('U001', '1', 'ARISTO CAESAR PRATAMA', '1', 1, '0852351191010', 'Aktif', '2021-12-06 08:33:24', '2021-12-03 01:50:51', '2021-12-05 14:19:21'),
+('U002', 'person', 'ANJAY GAMING', 'person', 2, '543534534', 'Aktif', '2021-12-06 07:53:33', '2021-12-03 01:54:41', '2021-12-06 07:52:27'),
 ('U004', 'a', 'ETST 2 DAS', 'a', 2, '12312', 'Non-Aktif', '2021-12-04 16:14:39', '2021-12-04 05:20:58', '2021-12-05 14:08:35');
 
 --
@@ -194,7 +198,7 @@ ALTER TABLE `barang`
 -- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`kode_kategori`),
+  ADD PRIMARY KEY (`id_kategori`),
   ADD UNIQUE KEY `nama_kategori` (`nama_kategori`);
 
 --
@@ -239,8 +243,8 @@ ALTER TABLE `users`
 -- Constraints for table `barang`
 --
 ALTER TABLE `barang`
-  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`kode_kategori`),
-  ADD CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`id_supplier`);
+  ADD CONSTRAINT `FK_kategori` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_supplier` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`id_supplier`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transaksi`
@@ -252,8 +256,8 @@ ALTER TABLE `transaksi`
 -- Constraints for table `transaksi_detail`
 --
 ALTER TABLE `transaksi_detail`
+  ADD CONSTRAINT `FK_barang` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_transaksi` FOREIGN KEY (`kode_transaksi`) REFERENCES `transaksi` (`kode_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaksi_detail_ibfk_2` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`),
   ADD CONSTRAINT `transaksi_detail_ibfk_3` FOREIGN KEY (`kode_transaksi`) REFERENCES `transaksi` (`kode_transaksi`);
 COMMIT;
 
